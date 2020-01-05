f0f8d3da0715088757c479dd51c8aad385e6f07e
rammatzkvosky <41573396+rammatzkvosky@users.noreply.github.com>
2020-01-05 13:54:02 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module modules
+
+go 1.13
+
+require (
+	github.com/chromedp/chromedp v0.5.2
+	github.com/donvito/hellomod v1.0.1
+)
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	 "github.com/donvito/hellomod"
+	 "github.com/chromedp/chromedp"
+)
+
+func main() {
+	 hellomod.SayHello()
+
+}
